413296db4f414bc96e49dd763c91efac6c7b5593
Alexey Min <alexey.min@gmail.com>
2018-06-14 00:18:26 +0300

    Do not hardcode device.. TODO: autodetect device

--- start_env.sh
+++ start_env.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# DEVICE=
+# ... --device=$DEVICE
+
 docker run \
     -it \
     --rm \
-    --device=/dev/bus/usb/001/005 \
     -v /home/halium:/home/halium \
     halium_build_env:latest
