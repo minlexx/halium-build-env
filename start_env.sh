f7f14d85d9e2fe905d7ba9325da040d1aa715cce
Alexey Min <alexey.min@gmail.com>
2018-06-14 00:31:34 +0300

    Pass device file as 1st parameter to starter script

--- start_env.sh
+++ start_env.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# DEVICE=
-# ... --device=$DEVICE
+# Usage:
+# ./start_env.sh /path/to/phone_device
+# ./start_env.sh /dev/bus/usb/001/006
+# TODO: autodetect device...
+
+DEVICE=$1
 
 docker run \
     -it \
     --rm \
+    --device=$DEVICE \
     -v /home/halium:/home/halium \
     halium_build_env:latest
