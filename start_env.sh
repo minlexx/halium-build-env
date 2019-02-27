04efc2a8722a097c3667f08ecfd139473c7df5bb
Alexey Min <alexey.min@gmail.com>
2019-02-27 14:44:37 +0300

    Make halium host source dir configurable

--- start_env.sh
+++ start_env.sh
@@ -4,10 +4,13 @@
 # ./start_env.sh
 # All USB devices are shared with container
 
+# Path to halium source root directory on host side
+HALIUM_HOST_HOME=/home/halium
+
 docker run \
     --privileged \
     -it \
     --rm \
     -v /dev/bus/usb:/dev/bus/usb \
-    -v /home/halium:/home/halium/buildroot \
+    -v $HALIUM_HOST_HOME:/home/halium/buildroot \
     halium_build_env:latest
